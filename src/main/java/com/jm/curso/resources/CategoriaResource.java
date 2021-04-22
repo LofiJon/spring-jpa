9785d19a755e4a998975581d0eea3d9231ad7706
Jonathan Malagueta <jonathan.malagueta@gmail.com>
2021-04-21 23:40:37 -0400

    inserindo domain categoria (model)

--- src/main/java/com/jm/curso/resources/CategoriaResource.java
+++ src/main/java/com/jm/curso/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.jm.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jm.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		 return "funcionando";
+	public List<Categoria> listar() {
+		 Categoria cat= new Categoria(1, "informatica");
+		 
+		 
+		 List<Categoria> lista = new ArrayList<>();
+		 lista.add(cat);
+		 
+		 return lista;
 	}
 
 }
